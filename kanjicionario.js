fa0d21b9f145e37b2a764382a27bd1e9e6d9bee5
Enzzx <bellottie815@gmail.com>
2023-05-29 00:47:54 -0300

    update

--- kanjicionario.js
+++ kanjicionario.js
@@ -1,13 +1,24 @@
+const formBtn = document.querySelector("#form-button")
+const form = document.querySelector(".form")
 
 class makeKanji {
     constructor(hira, roma, capt, nB) {
         document.body.children[3].appendChild(nB)
         nB.classList.add('kanji-box')
         nB.innerHTML = `
-          <p class="kanji-item">${hira}</p>
-          <p class="kanji-item">${roma}</p>
-          <p class="kanji-item">${capt}</p>
-      `
+          <aside>
+            <h3 class="kanji-item">Hiragana</h3>
+            <p class="kanji-caption">${hira}</p>
+        </aside>
+        <aside>
+            <h3 class="kanji-item">Romanji</h3>
+            <p class="kanji-caption">${roma}</p>
+        </aside>
+        <aside>
+            <h3 class="kanji-item">Caption</h3>
+            <p class="kanji-caption">${capt}</p>
+        </aside>
+        `
     }
 }
 
@@ -18,5 +29,15 @@ function createKanji() {
     const cap = document.querySelector("#caption").value
     const newBox = document.createElement("article")
 
-    const $kanji = new makeKanji(hir, rom, cap, newBox)
+    const kanji = new makeKanji(hir, rom, cap, newBox)
+
+    const formInterval = setInterval(() => {
+        form.classList.add("open")
+        clearInterval(formInterval)
+    }, 200)
+}
+
+function openForm() {
+    console.log("abrir")
+    form.classList.remove("open")
 }
